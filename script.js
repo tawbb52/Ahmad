01994f63092dee89868dc3e158f018ee34963ecb
tawbb52 <tawbb52@gmail.com>
2026-03-11 08:26:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-const STORAGE_KEY = "ahmad_ipa_apps";
-const storedApps = JSON.parse(localStorage.getItem(STORAGE_KEY));
-const appData = storedApps && storedApps.length ? storedApps : apps;
+// script.js
+
+// رقم الواتساب
+const phoneNumber = "966504106065";
+
+// جميع أزرار الواتساب
+document.querySelectorAll(".whatsapp-btn").forEach(button => {
+
+button.addEventListener("click", function(e){
+
+e.preventDefault();
+
+// جلب اسم المنتج
+const productName = this.getAttribute("data-product");
+
+// إنشاء الرسالة
+const message = `السلام عليكم، أرغب بطلب ${productName}`;
+
+// تحويل النص لرابط
+const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
+// فتح واتساب
+window.open(url, "_blank");
+
+});
+
+});
